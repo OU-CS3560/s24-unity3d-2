7491b4ff1cdd57427b839aface1832bde66e9722
pwnPHOfun <vuonghieunghia19122003@gmail.com>
2024-03-30 20:28:24 -0400

    destroyed blocks can now immediately affect the pathfinding

--- bomberman_game/Assets/Scripts/EnemySpawner.cs
+++ bomberman_game/Assets/Scripts/EnemySpawner.cs
@@ -5,21 +5,51 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] public GameObject CreepPrefab;
     [SerializeField] public GameObject player;
+    [SerializeField] public LayerMask grid_layer;
+
     private GameObject curCreep;
 
     public int width = 10;
     public int height = 4;
+    GameObject[] blocks;
 
     void Start()
     {
-        for (int y = 0; y < height; y += 2)
+        int maxY = 2, minY = -10;
+        int maxX = 5, minX = -7;
+        /*
+        for (int i = 0; i < this.blocks.Length; i++)
         {
-            for (int x = 0; x < width; x += 2)
+            var tmp = blocks[i].transform.position;
+            print(tmp.x);
+            if (maxY < tmp.y) maxY = (int)tmp.y;
+            if (maxX < tmp.x) maxX = (int)tmp.x;
+            if (minY > tmp.y) minY = (int)tmp.y;
+            if (minX > tmp.x) minX = (int)tmp.x;
+
+        }*/
+        // y is up/down a.k.a row
+        for (int y = minY; y < maxY; y += 2)
+        {
+            for (int x = minX; x < maxX; x += 2)
             {
-                curCreep = Instantiate(CreepPrefab, new Vector3(x, y, 0), Quaternion.identity);
-                curCreep.GetComponent<EnemyMovement>().player = this.player;
-                curCreep.GetComponent<Pathfinding.AIDestinationSetter>().target = this.player.transform;
+                if (!isBlock(x, y))
+                {
+                    curCreep = Instantiate(CreepPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    curCreep.GetComponent<EnemyMovement>().player = this.player;
+                    curCreep.GetComponent<Pathfinding.AIDestinationSetter>().target = this.player.transform;
+                }
             }
+        }
+    }
+    bool isBlock(int x, int y)
+    {
+        if (Physics2D.OverlapBox(new Vector2(x, y), Vector2.one / 2f, 0f, grid_layer)) { 
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
