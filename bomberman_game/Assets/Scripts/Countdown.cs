d23e63a629fe88809571f3c2141fc280c4306da6
minhOU93 <ml007921@ohio.edu>
2024-04-22 23:31:36 -0400

    music synced, menu reworked

--- bomberman_game/Assets/Scripts/Countdown.cs
+++ bomberman_game/Assets/Scripts/Countdown.cs
@@ -10,7 +10,7 @@ public class Countdown : MonoBehaviour
     public TextMeshProUGUI Ctext;
     public GameObject player1;
     public GameObject player2;
-    public GameObject timer;
+    //public GameObject timer;
     public GameObject screen;
 
     void Start()
@@ -22,18 +22,25 @@ public class Countdown : MonoBehaviour
     {
         while(time > 0)
         {
+
             Ctext.text = time.ToString();
 
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(1.22222f);
 
+            if(time == 1)
+            {
+                yield return new WaitForSeconds(0.3777777f);
+            }
+            
             time--;
+
         }
 
         Ctext.text = "GO!";
 
         player1.SetActive(true);
         player2.SetActive(true);
-        timer.SetActive(true);
+        //timer.SetActive(true);
 
 
         screen.SetActive(false);
